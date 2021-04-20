9e56ae039d7c923846344b5cf3fe7650a7a16bcc
Benjamin Fallar III <bfallar3@gmail.com>
2021-04-20 22:30:34 +0800

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cad-jcpremier';
+  title = '@cad-jcpremier';
+
+  goToktok() {
+    window.location.href = "http://www.toktok.ph/delivery/1111534896";
+  }
+
+  goMangBoks() {
+    window.location.href = "http://www.mangboks.ph/1111534896";
+  }
+
+  goBoyBondat() {
+    window.location.href = "http://www.boybondat.ph/ordernow/1111534896";
+  }
+
+  goSiomaiKing() {
+    window.location.href = "http://www.siomaiking.ph/ordernow/1111534896";
+  }
+
+  goCoppermask() {
+    window.location.href = "http://www.coppermask.ph/cpshop/1111534896";
+  }
+
+  goNinjaion() {
+    window.location.href = "http://www.ninjaph.com/cpshop/1111534896";
+  }
 }
